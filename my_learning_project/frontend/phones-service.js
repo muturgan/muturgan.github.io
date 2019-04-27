fbfb1b5405ad255f2f4beacafa4e19962218b757
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-27 15:15:16 +0300

    make phones-service shorter

--- my_learning_project/frontend/phones-service.js
+++ my_learning_project/frontend/phones-service.js
@@ -1,9 +1,7 @@
 export default class PhonesService {
    static getPhones(request = 'phones') {
-      let dataFromServer = fetch(`https://muturgan.github.io/my_learning_project/data/phones/${ request }.json`)
-         .then( response => { return response.json(); })
-         .catch( error => { console.error(error); });
-         
-      return dataFromServer;
+      return fetch(`https://muturgan.github.io/my_learning_project/data/phones/${ request }.json`)
+         .then( response => response.json())
+         .catch( error => console.error(error));
    }
 }

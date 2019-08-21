6413e51e6d0f0cc4a6e96fa1261ed621b4eaa5a0
Andrey Sakharov <a.sakharov@biletix.ru>
2019-08-21 15:48:45 +0300

    now is a pwa!

---
+++ barley-break/sw.js
@@ -0,0 +1,49 @@
+var CACHE = "pwabuilder-offline";
+
+var offlineFallbackPage = "index.html";
+
+self.addEventListener("install", function (event) {
+  console.info("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.info("[PWA Builder] Cached offline page during install");      
+      return cache.add(offlineFallbackPage);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.info("[PWA Builder] add page to offline cache: " + response.url);
+        event.waitUntil(updateCache(event.request, response.clone()));
+        return response;
+      })
+      .catch(function (error) {        
+        console.info("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}

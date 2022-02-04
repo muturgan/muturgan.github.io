d9317cf8a009bdaa26a748390aa9b7afcb57b711
Andrey Sakharov <sakharov.a@n-t.io>
2022-02-04 17:11:28 +0300

    refactoring

--- accords/js/script.js
+++ accords/js/script.js
@@ -1,119 +1,92 @@
 ﻿"use strict";
 
-let accordsList = [];
+(() => {
 
-
-let tone = {
+const tone = {
     tonic:           ['до', 'до-диез', 'ре','ре-диез', 'ми', 'фа', 'фа-диез', 'соль', 'соль-диез', 'ля', 'ля-диез', 'си'],
 	tonicShortEntry:   ['C',  'C#/D♭',   'D',  'D#/E♭',   'E',  'F',  'F#/G♭',    'G',   'G#/A♭',      'A',  'B',       'H'],
 	tonicAbstractValue:  [1,   1.5,       2,    2.5,       3,   4,     4.5,      5,       5.5,         6,   6.5,         7],
 	tonicArrayPosition:  [0,    1,        2,     3,        4,   5,      6,       7,        8,           9,   10,         11]
 };
 
-let upButton = document.getElementById("up");
-let downButton = document.getElementById("down");
+const upButton = document.getElementById("up");
+const downButton = document.getElementById("down");
+const accords = document.getElementsByName('accord');
 
 
-getAccordsList();
+const upAccordsList = () => {
 
+	accords.forEach((accord) => {
+		const accordInWork = accord.innerHTML;
 
-
-upButton.addEventListener("click", up);
-downButton.addEventListener("click", down);
-
-
-function up() {
-	getAccordsList();
-	upAccordsList();
-	insertAccordsList();
-}
-
-function down() {
-	getAccordsList();
-	downAccordsList();
-	insertAccordsList();
-}
-
-
-function getAccordsList() {
-	accordsList = [];
-
-	for (let i = 0; i < +document.getElementsByName('accord').length; i++) {
-		accordsList[i] = document.getElementsByName('accord')[i].innerHTML;
-	}
-}
-
-function upAccordsList() {
-
-	for (let i = 0; i < +accordsList.length; i++) {
 		let stringInWork = '';
 
 		let j = 0;
-		while (accordsList[i][0] != tone.tonicShortEntry[j]) {
+		while (accordInWork[0] != tone.tonicShortEntry[j]) {
 			j++;
 		}
 
-		if (accordsList[i][1] == '#') j++;
-		if (j > 11) j = 0;
-		if (accordsList[i][1] == ('♭' || 'b')) j--;
-		if (j < 0) j = 11;
+		if (accordInWork[1] === '#') {j++;}
+		if (j > 11) {j = 0;}
+		if (accordInWork[1] === ('♭' || 'b')) {j--;}
+		if (j < 0) {j = 11;}
 		j++;
-		if (j > 11) j = 0;
+		if (j > 11) {j = 0;}
 		if ((+tone.tonicAbstractValue[j] ^ 0) === +tone.tonicAbstractValue[j]) {
 			stringInWork = tone.tonicShortEntry[j];
 		} else {
-			stringInWork = accordsList[i][0] + '#';
+			stringInWork = accordInWork[0] + '#';
 		}
 
 		let k = 1;
-		if (accordsList[i][1] == '#' || accordsList[i][1] == '♭' || accordsList[i][1] == 'b') k = 2;
-		for (k; accordsList[i][k]; k++) {
-			stringInWork += accordsList[i][k];
+		const secondChar = accordInWork[1];
+		if (secondChar === '#' || secondChar === '♭' || secondChar === 'b') {k = 2;}
+		for (k; accordInWork[k]; k++) {
+			stringInWork += accordInWork[k];
 		}
-		accordsList[i] = stringInWork;
-	}
+
+		accord.innerHTML = stringInWork;
+	});
 }
 
-function downAccordsList() {
+const downAccordsList = () => {
 
-	for (let i = 0; i < +accordsList.length; i++) {
+	accords.forEach((accord) => {
+		const accordInWork = accord.innerHTML;
+
 		let stringInWork = '';
 
 		let j = 0;
-		while (accordsList[i][0] != tone.tonicShortEntry[j]) {
+		while (accordInWork[0] != tone.tonicShortEntry[j]) {
 			j++;
 		}
 
-		if (accordsList[i][1] == '#') j++;
-		if (j > 11) j = 0;
-		if (accordsList[i][1] == ('♭' || 'b')) j--;
-		if (j < 0) j = 11;
+		if (accordInWork[1] === '#') {j++;}
+		if (j > 11) {j = 0;}
+		if (accordInWork[1] === ('♭' || 'b')) {j--;}
+		if (j < 0) {j = 11;}
 		j--;
-		if (j < 0) j = 11;
+		if (j < 0) {j = 11;}
 		if ((+tone.tonicAbstractValue[j] ^ 0) === +tone.tonicAbstractValue[j]) {
 			stringInWork = tone.tonicShortEntry[j];
 		} else {
-			stringInWork = accordsList[i][0] + '♭';
+			stringInWork = accordInWork[0] + '♭';
 		}
-		if (stringInWork === 'H♭') stringInWork = 'B';
+		if (stringInWork === 'H♭') {stringInWork = 'B';}
 
 		let k = 1;
-		if (accordsList[i][1] == '#' || accordsList[i][1] == '♭' || accordsList[i][1] == 'b') k = 2;
+		const secondChar = accordInWork[1];
+		if (secondChar === '#' || secondChar === '♭' || secondChar === 'b') {k = 2;}
 
-		for (k; accordsList[i][k]; k++) {
-			stringInWork += accordsList[i][k];
+		for (k; accordInWork[k]; k++) {
+			stringInWork += accordInWork[k];
 		}
 
-		accordsList[i] = stringInWork;
-	}
+		accord.innerHTML = stringInWork;
+	});
 }
 
-function insertAccordsList() {
+upButton.addEventListener("click", upAccordsList);
+downButton.addEventListener("click", downAccordsList);
 
-	let accordInWork;
-
-	for (let i = 0; i < +document.getElementsByName('accord').length; i++) {
-		accordInWork = document.getElementsByName('accord')[i];
-		accordInWork.innerHTML = accordsList[i];
-	}
-}
+})();
